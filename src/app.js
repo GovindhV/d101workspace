75313c4d340fec6873d8555cde7e74d65763fcf7
govindh.v@gmail.com <govindh.v@gamil.com>
2021-11-30 12:27:57 -0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("Hello WORLD!\n");
+var express=requre('express');
+app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello world!\n');
+    
+});
+
+app.get('/mars', function(req, res){
+    res.send('Hello Mars!\n');
+});
+
+app.listen(8080, function() {
+console.log('examp app listening to the port 8080');
+});
